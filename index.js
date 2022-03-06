a968dd6ed86f38528ceecfb2a16e2d855e8bbf23
sham <86479073+shamchalase@users.noreply.github.com>
2022-03-06 13:57:05 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+const hamburger = document.querySelector(".hamburger");
+const menu = document.querySelector(".nav-list");
+
+humburger.addEventListener("click", () => {
+  menu.classList.toggle("open");
+});
